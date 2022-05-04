95344464b4bcea6b266f92f8194e7b5f46b029ac
fuen3 <bautiif2002@gmail.com>
2022-05-04 00:11:25 -0300

    gracias nahuxd

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 const contenedorTareas = document.getElementById("tareas")
 const btn = document.getElementById("btn-agregar")
 let arrayTareas = [];
-
+Local()
 btn.onclick = () =>{
-    const tareaAgregar = document.getElementById("text-agregar").value;
-    arrayTareas.push(tareaAgregar)
-    localStorage.setItem("tarea", JSON.stringify(arrayTareas));
-    let div = document.createElement("div")
-    let btn2 = document.createElement("button")
-    btn2.textContent = "Quitar";
-    div.textContent = tareaAgregar;
-    div.classList.add("tarea")
-    btn2.classList.add("btn")
-    contenedorTareas.append(div)
-    div.append(btn2)
-    btn2.onclick = () =>{
-        let temporal = tareaAgregar;
-        arrayTareas = JSON.parse(localStorage.getItem("tarea"));
-         let index = arrayTareas.indexOf(temporal)
-        console.log(index)
-        arrayTareas.splice(index,1)
+    if(arrayTareas === null){
+        arrayTareas = []
+    }
+    else{
+        const tareaAgregar = document.getElementById("text-agregar").value;
+        arrayTareas.push(tareaAgregar)
         localStorage.setItem("tarea", JSON.stringify(arrayTareas));
-        div.remove()
-    }   
-}
-
-function Local(arrayTareas){
-    arrayTareas = JSON.parse(localStorage.getItem("tarea"))
-    for (let index = 0; index < arrayTareas.length; index++) {
-        const tareaAgregar = arrayTareas[index];
         let div = document.createElement("div")
         let btn2 = document.createElement("button")
         btn2.textContent = "Quitar";
@@ -38,15 +19,43 @@ function Local(arrayTareas){
         contenedorTareas.append(div)
         div.append(btn2)
         btn2.onclick = () =>{
-            let temporal = tareaAgregar;
-            arrayTareas = JSON.parse(localStorage.getItem("tarea"));
-             let index = arrayTareas.indexOf(temporal)
-            console.log(index)
-            arrayTareas.splice(index,1)
-            localStorage.setItem("tarea", JSON.stringify(arrayTareas));
-            div.remove()
+        let temporal = tareaAgregar;
+        arrayTareas = JSON.parse(localStorage.getItem("tarea"));
+         let index = arrayTareas.indexOf(temporal)
+        console.log(index)
+        arrayTareas.splice(index,1)
+        localStorage.setItem("tarea", JSON.stringify(arrayTareas));
+        div.remove()
+    }   
+    }
+}
+
+function Local(){
+    arrayTareas = JSON.parse(localStorage.getItem("tarea"))
+    if(arrayTareas === null){
+        arrayTareas = []
+    }
+    else{
+        for (let index = 0; index < arrayTareas.length; index++) {
+            const tareaAgregar = arrayTareas[index];
+            let div = document.createElement("div")
+            let btn2 = document.createElement("button")
+            btn2.textContent = "Quitar";
+            div.textContent = tareaAgregar;
+            div.classList.add("tarea")
+            btn2.classList.add("btn")
+            contenedorTareas.append(div)
+            div.append(btn2)
+            btn2.onclick = () =>{
+                let temporal = tareaAgregar;
+                arrayTareas = JSON.parse(localStorage.getItem("tarea"));
+                 let index = arrayTareas.indexOf(temporal)
+                console.log(index)
+                arrayTareas.splice(index,1)
+                localStorage.setItem("tarea", JSON.stringify(arrayTareas));
+                div.remove()
+            }
         }
     }
 }
 
-Local(arrayTareas);
